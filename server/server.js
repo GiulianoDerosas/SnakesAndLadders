c6f1325020271b4833f9dae09105eac03f7f6d9a
GiulianoDerosas <giulianoderosas@gmail.com>
2021-05-21 10:17:15 +0400

    Created server file, adding base imports

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const cors = require("cors");
+app.use(cors());
+
+app.use(express.json());
+
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+
+MongoClient.connect('mongodb://localhost:27017')
+  .then((client) => {
+
+    // ADD DATABASE INFORMATION
+
+    app.listen(5000, function() {
+        console.log(`Server is now running`)})
+});
