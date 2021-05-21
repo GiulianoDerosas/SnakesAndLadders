6bf16b263e050d843e1610f0a9056b270e80a603
GiulianoDerosas <giulianoderosas@gmail.com>
2021-05-21 10:21:34 +0400

    Added imports to create_router and left template for express routing

---
+++ server/helpers/create_router.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const ObjectID = require('mongodb').ObjectID;
+
+const createRouter = function(collection) {
+
+    const router = express.Router();
+
+    // ADD ROUTER
+    // ADD ROUTER
+    // ADD ROUTER
+    
+    return router;
+};
+
+module.exports = createRouter;
