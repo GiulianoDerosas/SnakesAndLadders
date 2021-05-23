aae4e6e7041692e1a5f1e4ad17faa622376878dd
Steven Cormack <stevec@Stevens-MBP.lan>
2021-05-23 10:05:11 +0100

    board seperated from canvas

--- client/src/components/Canvas.js
+++ client/src/components/Canvas.js
@@ -1,55 +1,32 @@
 import React, { useRef, useEffect } from 'react'
+import drawBoard from "./GameBoard"
 
-const Canvas = () => {
-
-    const width = 500;
-    const height = 500;
-    const numCol = 10;
-    const numRow = 10;
-    const tileSize = 50;
-    let yAxis = (numRow - 1) * tileSize;
-    let xAxis = 0;
-    let board = [];
-    let direction = 1;
+const Canvas = ({board}) => {
     const canvasRef = useRef([])
 
-    const draw = ctx => {
-        for (let index = 0; index < numRow * numRow; index++) {
-            // add each tile to the array
-            board.push({ xAxis, yAxis, tileSize, index });
-            xAxis = xAxis + tileSize * direction;
-            // reverse direction after 10th tile and move up 1 tile
-            if (xAxis >= width || xAxis <= -tileSize) {
-                direction *= -1;
-                xAxis += tileSize * direction;
-                yAxis -= tileSize
-            }
+    const drawBoard = ctx => {
 
-            board.forEach(tile => {
-                ctx.fillStyle = '#ebebeb'
-                ctx.beginPath()
-                ctx.strokeRect(tile.xAxis, tile.yAxis, tile.tileSize, tile.tileSize)
-                ctx.fill()
-                ctx.font = "20px Arial";
-                ctx.strokeText(`${tile.index + 1}`, tile.xAxis, tile.yAxis + 50);
-                // if (tile.occupied) 
-            })
-
-            
-        }
+        board.forEach(tile => {
+            ctx.fillStyle = '#ebebeb'
+            ctx.beginPath()
+            ctx.strokeRect(tile.xAxis, tile.yAxis, tile.tileSize, tile.tileSize)
+            ctx.fill()
+            ctx.font = "15px Arial";
+            ctx.strokeText(`${tile.index + 1}`, tile.xAxis, tile.yAxis + 75);
+            // if (tile.occupied) 
+        })
     }
-
 
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
         //Our first draw
-        draw(context)
+        drawBoard(context)
         // context.fillStyle = '#ebebeb'
         // context.fillRect(0, 0, context.canvas.width, context.canvas.height)
-    }, [draw])
+    }, [drawBoard])
 
-    return <canvas width="500" height="500" ref={canvasRef} />
+    return <canvas width="750" height="750" ref={canvasRef} />
 }
 
 export default Canvas
--- client/src/components/GameBoard.js
+++ client/src/components/GameBoard.js
@@ -1,17 +1,31 @@
-import {useEffect, useRef} from "react"
+import { useEffect, useRef } from "react"
 import Canvas from "./Canvas"
 
 const GameBoard = () => {
 
-    useEffect(()=> {
+    const boardSize = 750;
+    const tiles = 10;
+    const tileSize = 75;
+    let yAxis = (tiles - 1) * tileSize;
+    let xAxis = 0;
+    let board = [];
+    let direction = 1;
 
-    })
-
-
+    for (let index = 0; index < tiles * tiles; index++) {
+        // add each tile to the array
+        board.push({ xAxis, yAxis, tileSize, index });
+        xAxis = xAxis + tileSize * direction;
+        // reverse direction after 10th tile and move up 1 tile
+        if (xAxis >= boardSize || xAxis <= -tileSize) {
+            direction *= -1;
+            xAxis += tileSize * direction;
+            yAxis -= tileSize
+        }
+    }
 
     return (
-        <Canvas />
-    
+
+        <Canvas board={board}/>
     )
 }
 
