d7255dd37be6c7a19998c5cd2fb99e8a3724b85b
Steven Cormack <stevec@Stevens-MBP.lan>
2021-05-26 19:07:42 +0100

    gui changes

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -10,7 +10,7 @@ const Tasks = ({randomTask, randomAction, noTask, previousPlayer, livePlayer}) =
         <h2 className="title">Player Tasks!</h2>
         <p className="fade-quote">{livePlayer.name}'s Roll</p>
         <br></br>
-        <p className="flashing-tasks">{previousPlayer.name}{randomTask}{randomAction}{noTask}</p>
+        <p className="flashing-tasks">{previousPlayer.name} {randomTask}{randomAction}{noTask}</p>
       </div>
     </div>
     )
